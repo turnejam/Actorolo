db972da79843daea1a776df4892942925968ac11
Will Turner <turnejam@gmail.com>
2018-05-28 14:45:15 -0400

    Joined Projects and Contacts, some view cleanup

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   belongs_to :user
 
   has_many :contact_projects
-  has_many :contacs, :through => :contact_projects
+  has_many :contacts, :through => :contact_projects
 
   enum category: [:play, :television, :film, :workshop, :other]
 end
